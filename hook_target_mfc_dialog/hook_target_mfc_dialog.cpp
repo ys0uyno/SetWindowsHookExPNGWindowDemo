a9a8b50c0bd20a2f2c8a9bbbc1ca01d1907e0c61
ys0uyno <ys0uyno@163.com>
2018-07-10 09:23:16 +0800

    delete useless code in hook_target_mfc_dialog

--- hook_target_mfc_dialog/hook_target_mfc_dialog.cpp
+++ hook_target_mfc_dialog/hook_target_mfc_dialog.cpp
@@ -4,7 +4,6 @@
 #include "stdafx.h"
 #include "hook_target_mfc_dialog.h"
 #include "util.h"
-#include "dlg_test0.h"
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
@@ -14,15 +13,6 @@ HHOOK g_hhook_wnd_ret = NULL;
 HHOOK g_hhook_msg = NULL;
 
 HWND g_hwnd = NULL;
-CBrush g_brush;
-BLENDFUNCTION g_blend;
-Gdiplus::Image *g_gdi_pimage;
-CRect g_crect_center;
-CDC *g_pmem_cdc;
-CBitmap *g_pcbitmap;
-int g_screen_x;
-int g_screen_y;
-dlg_test0 *m_pMainDlg;
 
 WNDPROC g_old_proc;
 static bool g_subclassed = false;
@@ -113,72 +103,6 @@ BOOL Chook_target_mfc_dialogApp::InitInstance()
 	return TRUE;
 }
 
-void ReSetChildDlg()
-{
-	CRect rcWin;
-	GetWindowRect(g_hwnd, &rcWin);
-	rcWin.left += g_crect_center.left;
-	rcWin.top += g_crect_center.top;
-	rcWin.right = rcWin.left + g_crect_center.Width();
-	rcWin.bottom = rcWin.top + g_crect_center.Height();
-	if (m_pMainDlg)
-	{
-		m_pMainDlg->MoveWindow(rcWin);
-	}
-}
-
-void DrawTXBar()
-{
-	RECT rct;
-	GetWindowRect(g_hwnd, &rct);
-
-	HDC hdcTemp = GetDC(g_hwnd);
-	HDC m_hdcMemory = CreateCompatibleDC(hdcTemp);
-	HBITMAP hBitMap = CreateCompatibleBitmap(hdcTemp, rct.right, rct.bottom);
-	SelectObject(m_hdcMemory, hBitMap);
-
-	POINT ptWinPos = { rct.left, rct.top };
-
-	Gdiplus::Graphics graphics(m_hdcMemory);
-	RECT rcClient;
-	GetClientRect(g_hwnd, &rcClient);
-
-	graphics.DrawImage(g_gdi_pimage, Gdiplus::Rect(rcClient.left, rcClient.top, rcClient.right, rcClient.bottom));
-
-	SIZE sizeWindow = { rct.right, rct.bottom };
-	POINT ptSrc = { 0, 0 };
-
-	DWORD dwExStyle = GetWindowLong(g_hwnd, GWL_EXSTYLE);
-	if ((dwExStyle & WS_EX_LAYERED) != WS_EX_LAYERED)
-		SetWindowLong(g_hwnd, GWL_EXSTYLE, dwExStyle ^ WS_EX_LAYERED);
-
-	HDC hdcScreen = GetDC(g_hwnd);
-	::UpdateLayeredWindow(g_hwnd, hdcScreen, &ptWinPos, &sizeWindow, m_hdcMemory, &ptSrc, 0, &g_blend, 2);
-	graphics.ReleaseHDC(m_hdcMemory);
-	::ReleaseDC(g_hwnd, hdcScreen);
-	hdcScreen = NULL;
-	::ReleaseDC(g_hwnd, hdcTemp);
-	hdcTemp = NULL;
-	DeleteObject(hBitMap);
-	DeleteDC(m_hdcMemory);
-	m_hdcMemory = NULL;
-}
-
-void set_image()
-{
-	GdiImageFromResource(&g_gdi_pimage, IDB_PNG_BK, L"PNG");
-	if (g_gdi_pimage)
-	{
-		CWnd *pcwnd = CWnd::FromHandle(g_hwnd);
-		pcwnd->MoveWindow(CRect(0, 0, g_gdi_pimage->GetWidth(), g_gdi_pimage->GetHeight()));
-		pcwnd->CenterWindow();
-		g_crect_center = CRect(31, 24, g_gdi_pimage->GetWidth() - 27, g_gdi_pimage->GetHeight() - 23);
-		ReSetChildDlg();
-		m_pMainDlg->SetDrawRect(g_crect_center);
-		DrawTXBar();
-	}
-}
-
 LRESULT CALLBACK new_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 {
 	if (Msg == WM_CTLCOLORBTN)
@@ -192,38 +116,10 @@ LRESULT CALLBACK new_proc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
 	if (Msg == WM_DESTROY)
 	{
 		g_subclassed = false;
-
-		// from mfc_window1Dlg
-		if (g_pmem_cdc)
-		{
-			delete g_pmem_cdc;
-			g_pmem_cdc = NULL;
-		}
-
-		if (g_pcbitmap)
-		{
-			delete g_pcbitmap;
-			g_pcbitmap = NULL;
-		}
-
-		// from mfc_window1's dlg_parent
-		if (m_pMainDlg)
-		{
-			delete m_pMainDlg;
-			m_pMainDlg = NULL;
-		}
-
-		if (g_gdi_pimage)
-		{
-			delete g_gdi_pimage;
-			g_gdi_pimage = NULL;
-		}
 	}
 
 	if (Msg == WM_MOVE)
 	{
-		OutputDebugString(L"new_proc WM_MOVE");
-		ReSetChildDlg();
 	}
 
 	return CallWindowProc(g_old_proc, hWnd, Msg, wParam, lParam);
@@ -237,25 +133,10 @@ LRESULT CALLBACK GetMsgProc(int nCode, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_PAINT:
 		{
-			OutputDebugString(L"WH_GETMESSAGE WM_PAINT");
-
-			if (g_hwnd)
-			{
-				CDC *pDC = CDC::FromHandle(GetDC(g_hwnd));
-				CRect rect;
-				GetClientRect(g_hwnd, rect);
-				CBitmap *oldBitmap = g_pmem_cdc->SelectObject(g_pcbitmap);
-				pDC->BitBlt(rect.left, rect.top, g_screen_x, g_screen_y, g_pmem_cdc, rect.left, rect.top, SRCCOPY);
-				g_pmem_cdc->SelectObject(oldBitmap);
-			}
 		}
 		break;
 	case WM_LBUTTONDOWN:
 		{
-			if (g_hwnd == p->hwnd)
-			{
-				PostMessage(p->hwnd, WM_NCLBUTTONDOWN, HTCAPTION, p->lParam);
-			}
 		}
 		break;
 	}
@@ -271,12 +152,6 @@ LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 		{
-			if (g_subclassed)
-			{
-				break;
-			}
-			g_subclassed = true;
-
 			OutputDebugString(L"WM_INITDIALOG");
 
 			Gdiplus::GdiplusStartupInput input;
@@ -291,38 +166,6 @@ LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
 			}
 
 			g_hwnd = hwnd;
-
-			// OnCreate
-			CDC *pDC = CDC::FromHandle(GetDC(hwnd));
-			g_pmem_cdc = new CDC();
-			g_pcbitmap = new CBitmap();
-			g_screen_x = 556;
-			g_screen_y = 397;
-			g_pmem_cdc->CreateCompatibleDC(pDC);
-			g_pcbitmap->CreateCompatibleBitmap(pDC, g_screen_x, g_screen_y);
-			CBitmap *oldBitmap = g_pmem_cdc->SelectObject(g_pcbitmap);
-			CBrush brush(RGB(255, 255, 255));
-			CRect rect;
-			GetClientRect(hwnd, &rect);
-			g_pmem_cdc->FillRect(CRect(rect.left, rect.top, g_screen_x, g_screen_y), &brush);
-			g_pmem_cdc->SelectObject(oldBitmap);
-			ReleaseDC(hwnd, pDC->GetSafeHdc());
-
-			// fix "Debug Assertion Failed!" at dlgcore.cpp:213
-			if (g_rh.load(L"hook_target_mfc_dialog"))
-			{
-				AfxSetResourceHandle(g_rh.get_hinstance());
-
-				m_pMainDlg = new dlg_test0(CWnd::FromHandle(hwnd));
-				if (m_pMainDlg)
-				{
-					// here will cause dlgcore.cpp:213 "Debug Assertion Failed!"
-					// ERROR: Cannot find dialog template with IDD IDD_DIALOG_TEST0
-					m_pMainDlg->Create(IDD_DIALOG_TEST0, CWnd::FromHandle(hwnd));
-					m_pMainDlg->ShowWindow(SW_SHOW);
-					m_pMainDlg->SetParentDlg(CWnd::FromHandle(hwnd));
-				}
-			}
 
 			DWORD dwstyle = GetWindowLong(hwnd, GWL_STYLE);
 			DWORD dwstyle_new = WS_OVERLAPPED
@@ -341,29 +184,6 @@ LRESULT CALLBACK CallWndRetProc(int nCode, WPARAM wParam, LPARAM lParam)
 			SetWindowLong(hwnd, GWL_EXSTYLE, dwexstyle_new);
 
 			SetWindowPos(hwnd, NULL, 0, 0, 0, 0, SWP_NOZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
-
-			// OnInitDialog
-			COLORREF transColor = RGB(0, 255, 0);
-			g_brush.CreateSolidBrush(transColor);
-
-			DWORD dwStyle = GetWindowLong(hwnd, GWL_STYLE);
-			SetWindowLong(hwnd, GWL_STYLE, dwStyle | WS_EX_LAYERED);
-
-			// When bAlpha is 0, the window is completely transparent,
-			// When bAlpha is 255, the window is opaque.
-			SetLayeredWindowAttributes(hwnd, transColor, 0, LWA_COLORKEY);
-
-			dwStyle = GetWindowLong(hwnd, GWL_STYLE);
-
-			/*VERIFY((dwStyle & WS_POPUP) != 0);*/ // cause "Debug Assertion Failed!"
-			VERIFY((dwStyle & WS_CHILD) == 0);
-
-			g_blend.BlendOp = 0;
-			g_blend.BlendFlags = 0;
-			g_blend.AlphaFormat = 1;
-			g_blend.SourceConstantAlpha = 255;
-
-			set_image();
 
 			g_old_proc = (WNDPROC)SetWindowLong(hwnd, GWL_WNDPROC, (LONG)new_proc);
 		}
